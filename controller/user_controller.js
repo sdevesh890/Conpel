8c7d4bdceb76d45d9cfc064fc3503db90e0aac06
sdevesh890 <74634378+sdevesh890@users.noreply.github.com>
2023-03-12 14:52:05 +0530

    added sign-in feature

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -20,9 +20,35 @@ module.exports.profile = function(req ,res)
 }
 module.exports.create = function(req , res)
 {
+    if(req.body.user_pass != req.body.user_conf_pass)
+    {
+       return res.redirect('back');
+    }
+    User.findOne({email : req.body.user_email }).then(user=>
+        {
+            if(!user)
+            {
+                User.create(
+                    {
+                        name : req.body.user_name , 
+                        email : req.body.user_email ,
+                        password : req.body.user_pass
+                    }
+                ).then(()=> 
+                    {
+                        return res.redirect('/user/sign-in');
+                    })
+            }else 
+            {
+                return res.redirect('back');
+            }
+        }
+    ).catch(err=>{
+        console.log('Error occuired while finding user',err); 
+    });
 
 }
 module.exports.create_session = function(req , res)
 {
-
+ 
 }
